cc9a31491194a0b9206c666667fbc674e7292576
devsecopscloset <106809417+devsecopscloset@users.noreply.github.com>
2022-10-31 10:34:36 +0530

    Update - 2

--- Archer-automation.py
+++ Archer-automation.py
@@ -4,14 +4,31 @@ excel_name = 'Archer Dummy Data.xlsx'
 excel_sheet_name = 'Archer Search Report'
 column_name = 'Finding Name'
 
-# Step 1 : create a function to read an excel file based on dynamic inputs
-excel_data = read_excel(excel_name, excel_sheet_name)
+# check if the specific excel file exists
+excel_exists = if_file_exists(excel_name)
 
-# Step 2 : create a function to read a certain column in an excel sheet
-excel_column_data = read_column(column_name, excel_data)
+if excel_exists != True:
 
-# Step 3 : create a function to obtain unique values in a dataset
-unique_datasets = get_unique_values(excel_column_data)
+    func_bar(100, "Searching for the excel file...")
+    print(" The specified Excel file does not exist.")
+    print(" Program exiting now.")
 
-# Step 4 : create a function to create excel sheets based on unique issues found
-create_unique_excels(unique_datasets, column_name, excel_name, excel_sheet_name)
+else :
+
+    func_bar(100, "Searching for the excel file...")
+    print(" The specified excel file is found.")
+    print(" Data Analysis initiating...")
+
+    # Step 1 : create a function to read an excel file based on dynamic inputs
+    excel_data = read_excel(excel_name, excel_sheet_name)
+
+    # Step 2 : create a function to read a certain column in an excel sheet
+    excel_column_data = read_column(column_name, excel_data)
+
+    # Step 3 : create a function to obtain unique values in a dataset
+    unique_datasets = get_unique_values(excel_column_data)
+
+    # Step 4 : create a function to create excel sheets based on unique issues found
+    create_unique_excels(unique_datasets, column_name, excel_name, excel_sheet_name)
+
+    print(" Data Analysis Done.")
